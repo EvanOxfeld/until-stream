bca72bf32173a01c5fc9f65531bb9f2bf1cf60af
Evan Oxfeld <eoxfeld@nearinfinity.com>
2013-02-17 00:45:07 -0500

    Add tests

--- test/untilStreamTest.js
+++ test/untilStreamTest.js
@@ -56,4 +56,58 @@ test("pipe until pattern", function (t) {
   sourceStream.pipe(us).pipe(writableStream);
 });
 
-//todo pipe until pattern divided between chunks
+test("pipe until pattern - pattern straddles two chunks", function (t) {
+  t.plan(2);
+  var us = new UntilStream({ pattern: ' World'});
+  us.on('finish', function () {
+    sourceStream.destroy();
+  });
+
+  var sourceStream = new streamBuffers.ReadableStreamBuffer({
+    frequency: 0,
+    chunkSize: 1000
+  });
+  sourceStream.put("Hello World!");
+
+  var writableStream = new streamBuffers.WritableStreamBuffer({
+    initialSize: 100
+  });
+
+  writableStream.on('close', function () {
+    var str = writableStream.getContentsAsString('utf8');
+    t.equal(str, 'Hello');
+    var data = us.read();
+    t.equal(data.toString(), ' World!');
+    t.end();
+  });
+
+  sourceStream.pipe(us).pipe(writableStream, { chunkSize: 8 });
+});
+
+test("pipe until pattern - first chunk ends with potential pattern", function (t) {
+  t.plan(2);
+  var us = new UntilStream({ pattern: ' World'});
+  us.on('finish', function () {
+    sourceStream.destroy();
+  });
+
+  var sourceStream = new streamBuffers.ReadableStreamBuffer({
+    frequency: 0,
+    chunkSize: 1000
+  });
+  sourceStream.put("Hello Wordy World!");
+
+  var writableStream = new streamBuffers.WritableStreamBuffer({
+    initialSize: 100
+  });
+
+  writableStream.on('close', function () {
+    var str = writableStream.getContentsAsString('utf8');
+    t.equal(str, 'Hello Wordy');
+    var data = us.read();
+    t.equal(data.toString(), ' World!');
+    t.end();
+  });
+
+  sourceStream.pipe(us).pipe(writableStream, { chunkSize: 8 });
+});
--- until.js
+++ until.js
@@ -3,7 +3,7 @@
 module.exports = Until;
 var PassThrough = require('stream').PassThrough;
 var inherits = require("util").inherits;
-require('bufferjs');
+var Buffers = require('buffers');
 
 if (!PassThrough) {
   PassThrough = require('readable-stream/passthrough');
@@ -13,7 +13,8 @@ inherits(Until, PassThrough);
 
 //todo only emit end when buffer is empty
 function Until(opts) {
-  this._buf = null;
+  this._buf = Buffers();
+  this._found = false;
 
   if (typeof opts.pattern === "string") {
     opts.pattern = new Buffer(opts.pattern);
@@ -21,10 +22,10 @@ function Until(opts) {
   this._opts = opts;
   this.unpiping = false;
 
+  //todo allow and handle normal PassThrough opts
   PassThrough.call(this);
 }
 
-//todo buffer between chunks
 Until.prototype.read = function (size) {
   if (this.unpiping) {
     //either return null from read() until everything is unpiped
@@ -32,21 +33,46 @@ Until.prototype.read = function (size) {
     return null;
   }
 
+  if (size && size <= this._buf.length) {
+    var data = this._buf.slice(0, size);
+    this._buf = Buffers(this._buf.slice(size, this._buf.length));
+    return data;
+  }
+
   if (size) {
+    size = size - this._buf.length;
+  }
+
+  //todo override pipe instead of this hack
+  if (size !== null && size !== undefined && !this._readableState.pipeChunkSize) {
+    //todo other states that just use PassThrough's read?
     //todo start from buf
     return PassThrough.prototype.read.call(this, size);
   }
 
-  var data = PassThrough.prototype.read.call(this);
+  var data = size ? PassThrough.prototype.read.call(this, size) : PassThrough.prototype.read.call(this);
   if (data) {
-    var index = data.indexOf(this._opts.pattern);
-    if (index === -1) {
-      return data;
+    var output = this._buf;
+    output.push(data);
+    var index = output.indexOf(this._opts.pattern);
+    if (index === -1 || (this._found && index === 0)) {
+      this._found = false;
+      //slice off pattern.length - 1 from the end in case the pattern straddles chunks
+      var offset = this._readableState.length ? output.length - this._opts.pattern.length + 1 : -1;
+      if (offset > 0) {
+        this._buf = Buffers([output.slice(offset, output.length)]);
+        output = output.slice(0, offset);
+      } else {
+        output = output.toBuffer();
+      }
+
+      return output;
     }
 
     //lop off everything starting with pattern & put it in buffer for next time
-    this._buf = data.slice(index, data.length);
-    var result = data.slice(0, index)
+    this._found = true;
+    this._buf = Buffers([output.slice(index, output.length)]);
+    var result = output.slice(0, index);
     var self = this;
     if (this._readableState.pipesCount) {
       this.unpiping = true;
@@ -55,9 +81,10 @@ Until.prototype.read = function (size) {
 
     return result;
   } else {
-    data = this._buf;
-    this._buf = null;
-    return data;
+    data = this._buf.toBuffer();
+    this._buf = Buffers();
+    var output = data.length ? data : null;
+    return output;
   }
 
   function endPipes() {
