fc66abf291b9310bb768817781380cfb54e9ddd3
Evan Oxfeld <eoxfeld@nearinfinity.com>
2013-02-21 08:45:43 -0500

    Pipe until pattern example

---
+++ examples/pipeUntilPattern.js
@@ -0,0 +1,25 @@
+var UntilStream = require('../');
+var streamBuffers = require("stream-buffers");
+
+var us = new UntilStream({ pattern: ' World'});
+
+var sourceStream = new streamBuffers.ReadableStreamBuffer({ chunkSize: 1000 });
+sourceStream.put("Hello World!");
+us.on('finish', function () {
+  sourceStream.destroy();
+});
+
+var writableStream = new streamBuffers.WritableStreamBuffer();
+
+sourceStream.pipe(us).pipe(writableStream);
+
+writableStream.once('close', function () {
+  var str = writableStream.getContentsAsString('utf8'); //Hello
+  console.log('Piped data before pattern occurs:', "'" + str + "'");
+  var data = us.read(); //data starts with pattern
+  console.log('Next call to read() starts with pattern:', "'" + data.toString() + "'");
+});
+
+//Output
+//Piped data before pattern occurs: 'Hello'
+//Next call to read() starts with pattern: ' World!'
