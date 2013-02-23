bb6994157bc1ad1613b5afb809bfd00abc9147d8
Evan Oxfeld <eoxfeld@nearinfinity.com>
2013-02-23 00:50:55 -0500

    Clean up examples

--- examples/pipeUntilPattern.js
+++ examples/pipeUntilPattern.js
@@ -1,10 +1,10 @@
 var UntilStream = require('../');
 var streamBuffers = require("stream-buffers");
 
-var us = new UntilStream({ pattern: ' World'});
+var us = new UntilStream({ pattern: 'World'});
 
-var sourceStream = new streamBuffers.ReadableStreamBuffer({ chunkSize: 1000 });
-sourceStream.put("Hello World!");
+var sourceStream = new streamBuffers.ReadableStreamBuffer();
+sourceStream.put("Hello World");
 us.on('finish', function () {
   sourceStream.destroy();
 });
@@ -14,12 +14,12 @@ var writableStream = new streamBuffers.WritableStreamBuffer();
 sourceStream.pipe(us).pipe(writableStream);
 
 writableStream.once('close', function () {
-  var str = writableStream.getContentsAsString('utf8'); //Hello
+  var str = writableStream.getContentsAsString('utf8');
   console.log('Piped data before pattern occurs:', "'" + str + "'");
-  var data = us.read(); // World
+  var data = us.read();
   console.log('Next call to read() returns the pattern:', "'" + data.toString() + "'");
 });
 
 //Output
-//Piped data before pattern occurs: 'Hello'
-//Next call to read() returns the pattern: ' World'
+//Piped data before pattern occurs: 'Hello '
+//Next call to read() returns the pattern: 'World'
