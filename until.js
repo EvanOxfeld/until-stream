31489ab267a1deae1775e5063e1cec6d6307e7c4
Evan Oxfeld <eoxfeld@nearinfinity.com>
2013-02-23 01:34:55 -0500

    Cleanup / better flush behavior before stream end
    Ensure all buffered data is written to pipes prior
    to emitting 'end'

--- until.js
+++ until.js
@@ -12,9 +12,7 @@ if (!PassThrough) {
 inherits(Until, PassThrough);
 
 function Until(opts) {
-  this._buf = Buffers();
-  this._matched = false;
-
+  //todo allow pattern to be set later
   if (typeof opts.pattern === "string") {
     opts.pattern = new Buffer(opts.pattern);
   } else if (!opts.pattern instanceof Buffer) {
@@ -22,17 +20,12 @@ function Until(opts) {
   }
 
   this._opts = opts;
+  this._buf = Buffers();
+  this._flushing = false;
   this.unpiping = false;
 
   //todo allow and handle normal PassThrough opts
   PassThrough.call(this);
-}
-
-Until.prototype._flush = function(output, cb) {
-  if (!this._readableState.length) {
-    endPipes.call(this, this._buf.toBuffer());
-  }
-  cb();
 }
 
 Until.prototype.read = function (size) {
@@ -42,73 +35,100 @@ Until.prototype.read = function (size) {
     return null;
   }
 
+  var rs = this._readableState;
   var pattern = this._opts.pattern;
-  var bufLength = this._buf.length;
-  if (this._matched || this._buf.indexOf(pattern) === 0
-    || this._buf.push(PassThrough.prototype.read.call(this, size) || new Buffer(0))
+  var data;
+  if (this._buf.indexOf(pattern) === 0  ||
+      (data = PassThrough.prototype.read.call(this, size)) && this._buf.push(data)
         && this._buf.indexOf(pattern) === 0) {
-    if (this._readableState.pipesCount && !this.unpiping) {
+    if (rs.pipesCount) {
       this.unpiping = true;
       endPipes.call(this);
       return null;
     }
-    this._matched = false;
     var output = this._buf.slice(0, pattern.length);
-    var newBuf = Buffers([this._buf.slice(pattern.length)]);
-    this._buf = newBuf;
-    return output;
-  }
-
-  if (bufLength && bufLength === this._buf.length && !this._readableState.pipesCount) {
-    var output = this._buf.toBuffer();
-    this._buf = Buffers();
+    this._buf = Buffers([this._buf.slice(pattern.length)]);
     return output;
   }
 
   var index = this._buf.indexOf(pattern);
   if (index > 0) {
-    //lop off everything starting with pattern & put it in buffer for next time
-    this._matched = true;
+    //lop off everything starting with pattern & put it in buffer for next read()
     var output = this._buf.slice(0, index);
     this._buf = Buffers([this._buf.slice(index)]);
-    var self = this;
-    if (this._readableState.pipesCount) {
+    if (rs.pipesCount) {
       this.unpiping = true;
       process.nextTick(endPipes.bind(this));
     }
     return output;
   }
 
+  if (pattern.length > this._buf.length) {
+    if (this._flushing) {
+      var output = this._buf.toBuffer();
+      this._buf = Buffers();
+      return output;
+    }
+    return null;
+  }
+
   //slice off pattern.length - 1 from the end in case the pattern straddles chunks
-  var offset = this._buf.length - pattern.length + 1;
+  var offset = this._buf.length - (pattern.length - 1);
   var output = this._buf.slice(0, offset);
   output = output.length ? output : null;
   this._buf = offset > 0 ? Buffers([this._buf.slice(offset)]) : this._buf;
   return output;
 };
 
-function endPipes(data) {
-  var pipes = this._readableState.pipes;
-  var pipesCount = this._readableState.pipesCount;
-  this.unpipe();
-  this.unpiping = false;
-
-
-  switch (pipesCount) {
-    case 0:
-      //no pipes to end
-      break;
-    case 1:
-      pipes.end(data || "");
-      break;
-    default:
-      //todo
-      break;
-    }
-}
-
 Until.prototype.pipe = function(dest, pipeOpts) {
   pipeOpts = pipeOpts || {};
   pipeOpts.end = pipeOpts.end || false;
   return PassThrough.prototype.pipe.call(this, dest, pipeOpts)
 };
+
+Until.prototype._flush = function(output, cb) {
+  this._flushing = true;
+  var rs = this._readableState;
+  if (rs.length || this._buf.length) {
+    if (rs.pipesCount > 0) {
+      writePipes.call(this, this.read());
+    }
+    //allow for I/O
+    return setImmediate(this._flush.bind(this, output, cb));
+  }
+  endPipes.call(this);
+  process.nextTick(cb);
+}
+
+function writePipes(data) {
+  var rs = this._readableState;
+  switch (rs.pipesCount) {
+  case 0:
+    break;
+  case 1:
+    rs.pipes.write(data);
+    break;
+  default:
+    //todo
+    break;
+  }
+}
+
+function endPipes() {
+  var rs = this._readableState;
+  var pipes = rs.pipes;
+  var pipesCount = rs.pipesCount;
+  this.unpipe();
+  this.unpiping = false;
+
+  switch (pipesCount) {
+  case 0:
+    break;
+  case 1:
+    pipes.end();
+    break;
+  default:
+    //todo
+    break;
+  }
+}
